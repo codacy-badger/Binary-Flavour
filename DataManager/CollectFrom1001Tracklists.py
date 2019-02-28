cbb26ddc0d9d4489dad4f121392e391bd2489da8
byun <byunjuneseok@gmail.com>
2019-02-28 15:36:45 +0900

    // TDD (Unittest)

--- DataManager/CollectFrom1001Tracklists.py
+++ DataManager/CollectFrom1001Tracklists.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
-import TrackPlaylist
+from bs4 import BeautifulSoup
+import urllib.request
+import urllib.parse
+
 
 URL_1001TRACKLISTS_LIST_YEAR_HEAD = 'https://www.1001tracklists.com/year/2019/index'
 URL_1001TRACKLISTS_LIST_YEAR_TAIL = '.html'
@@ -10,7 +13,7 @@ def getCurrentListYearURL():
     CurrentUrl = URL_1001TRACKLISTS_LIST_YEAR_HEAD + URL_1001TRACKLISTS_LIST_YEAR_TAIL
     return CurrentUrl
 
-def CollectFromListYear(year):
+def CollectFromListYearSelenium(year):
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
     options.add_argument('window-size=1920x1080')
@@ -18,10 +21,12 @@ def CollectFromListYear(year):
     driver = webdriver.Chrome('chromedriver', chrome_options=options)
 
     BaseURL = 'https://www.1001tracklists.com/year/' + str(year) + '/index.html'
-    XPATH_MAX_INDEX = '// *[ @ id = "middleTbl"] / tbody / tr / td / ul / li[13] / a'
+    print("Base URL was generated.", BaseURL)
+    XPATH_MAX_INDEX = '//*[@id="middleTbl"]/tbody/tr/td/ul/li[13]/a'
     MAX_INDEX = 0
 
     driver.get(BaseURL)
     driver.implicitly_wait(3)
-    MAX_INDEX = driver.find_elements_by_xpath(XPATH_MAX_INDEX)
-    print(MAX_INDEX)
+    MAX_INDEX = driver.find_element_by_xpath(XPATH_MAX_INDEX)
+    print(MAX_INDEX, MAX_INDEX.text)
+    return int(MAX_INDEX.text)
